edb505ddee40ec23594d757a9552ac8ccde28869
Tosini13 <jbartos13@gmail.com>
2023-08-03 20:16:26 +0200

    feat: add docker-compose and refresh backend after changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,9 +7,9 @@ const app: Express = express();
 const port = process.env.PORT;
 
 app.get("/", (_req: Request, res: Response) => {
-  res.send("Hello world! XD");
+  res.send("Hello world!");
 });
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`[server]: Server is running at http://localhost:${port} or not`);
 });
